8b8a8db1fb17dee628ea062f89564965b1f4782a
Eduardo <b3.3d@hotmail.com>
2018-05-30 15:06:44 +0200

    A new application. Added three API:s

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = angular.module('App', ['ngRoute', 'firebase']);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/home.html',
+      controller: 'FirstController'
+    })
+
+    .when('/sign-up', {
+      templateUrl: 'templates/sign-up.html',
+      controller: 'SignUpController'
+    })
+    .when('/login', {
+      templateUrl: 'templates/login.html',
+      controller: 'LoginController'
+    })
+    // Added code for contact
+    .when('/contact', {
+      templateUrl: 'templates/contact.html',
+      controller: 'ContactController'
+    });
+});
